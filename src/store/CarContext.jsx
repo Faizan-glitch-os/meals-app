a7969ec08ff15e8ee2b29bbdb87df2e4573f96d7
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-05-07 12:19:57 +0500

    reducer function now has removing item functionality, everything is set about context and its usage, now it can be used everywhere

--- src/store/CarContext.jsx
+++ src/store/CarContext.jsx
@@ -31,13 +31,48 @@ function cartReducer(state, action) {
   }
 
   if (action.type == "REMOVE_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedItems = [...state.items];
+
+    if (existingCartItem.quantity === 1) {
+      updatedItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return { ...state, items: updatedItems };
   }
+
+  return state;
 }
 
 export function CartContextProvider({ children }) {
-  useReducer(cartReducer, { items: [] });
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, {
+    items: [],
+  });
 
-  return <CartContext.Provider>{children}</CartContext.Provider>;
+  function addItem(item) {
+    dispatchCartAction({ item, type: "ADD_ITEM" });
+  }
+
+  function removeItem(id) {
+    dispatchCartAction({ id, type: "REMOVE_ITEM" });
+  }
+
+  const cartContext = { items: cartState.items, addItem, removeItem };
+
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
 }
 
 export default CartContext;
