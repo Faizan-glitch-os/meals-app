d0091eb6330d0d37f92996b4414d6a4cdf0ca82e
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-05-09 11:00:09 +0500

    SuccessOrder component also fixed

--- src/components/SuccessOrder.jsx
+++ src/components/SuccessOrder.jsx
@@ -1,13 +1,13 @@
 import Modal from "./Modal";
 import Button from "./ui/Button";
 
-export default function SuccessOrder({ onClick, onOpen, onClose }) {
+export default function SuccessOrder({ open, onClose }) {
   return (
-    <Modal open={onOpen} onClose={onClose}>
+    <Modal open={open} onClose={onClose}>
       <h2>Success</h2>
       <p>Your order was submitted successfully</p>
       <p className="modal-actions">
-        <Button onClick={onClick}>Okay</Button>
+        <Button onClick={onClose}>Okay</Button>
       </p>
     </Modal>
   );
